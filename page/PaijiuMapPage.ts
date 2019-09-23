5ea9d9d90c631a41ca7f075ff1de55b3716959a3
happy <happy>
2019-09-23 20:09:24 +0800

    牌九

--- page/PaijiuMapPage.ts
+++ page/PaijiuMapPage.ts
@@ -127,6 +127,7 @@ module gamepaijiu.page {
                 this._paijiuMgr.on(PaijiuMgr.CONTINUE_MATCH, this, this.onContinueGame);
             }
             this._game.playMusic(Path_game_paijiu.music_paijiu + MUSIC_PATH.bgMusic);
+            this._viewUI.btn_cards.left = this._game.isFullScreen ? 110 : 90;
             this._viewUI.btn_menu.left = this._game.isFullScreen ? 30 : 10;
             this._viewUI.img_menu.left = this._game.isFullScreen ? 25 : 10;
         }

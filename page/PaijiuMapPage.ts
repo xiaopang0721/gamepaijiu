979fd7c33155469b910db4d930c2970c7a22f648
xiaoer <xiaoer@example.com>
2019-10-18 12:02:08 +0800

    修改当祈福动画飘过去后，在显示祈福头像

--- page/PaijiuMapPage.ts
+++ page/PaijiuMapPage.ts
@@ -413,10 +413,11 @@ module gamepaijiu.page {
                                 viewHead.img_qifu.visible = true;
                                 viewHead.img_icon.skin = TongyongUtil.getHeadUrl(unit.GetHeadImg());
                             })
-                        } else {
-                            viewHead.img_qifu.visible = true;
-                            viewHead.img_icon.skin = TongyongUtil.getHeadUrl(unit.GetHeadImg());
-                        }
+                        } 
+                        // else {
+                        //     viewHead.img_qifu.visible = true;
+                        //     viewHead.img_icon.skin = TongyongUtil.getHeadUrl(unit.GetHeadImg());
+                        // }
                     } else {
                         viewHead.img_qifu.visible = false;
                         viewHead.img_icon.skin = TongyongUtil.getHeadUrl(unit.GetHeadImg());
